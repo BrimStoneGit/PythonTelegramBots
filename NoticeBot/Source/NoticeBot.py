dfc2257a1dc2bb804e7c57d8fc86cacebd129421
BENNY-LAPTOP\Benny <benjamindornfeld@gmx.de>
2017-04-20 14:52:53 +0200

    Heureka! It works!

--- NoticeBot/Source/NoticeBot.py
+++ NoticeBot/Source/NoticeBot.py
@@ -5,6 +5,9 @@ import telepot.namedtuple
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
 def handle(msg):
+    global yesNoKeyboard
+    global myNotice
+    global sureToSave
     content_type, chat_type, chat_id = telepot.glance(msg)
     chat_id = msg['chat']['id']
     if (content_type == 'text') :
@@ -16,20 +19,52 @@ def handle(msg):
     
     print (chat_id)
     
-    messageList = message.split(' ', 1)
-    if messageList[0] == '/NewNotice' :
-        myMessage = bot.sendMessage(chat_id, 'Deine Notiz lautet: ' + messageList[1])
-        bot.sendMessage(chat_id, 'Soll ich diese Notiz aufnehmen?', 'Markdown', False, False, myMessage['message_id'], yesNoKeyboard)
+    if sureToSave :
+        if message == '/yes' :
+            bot.sendMessage(chat_id, 'Notiz hinzugefügt')
+            f = open(str(chat_id) + '.txt', 'a+')
+            f.write(myNotice + '\n')
+            f.close()
+            myNotice = ''
+            sureToSave = False
+        else :
+            bot.sendMessage(chat_id, 'Notiz verworfen')
+            myNotice = ''
+            sureToSave = False
     else :
-        bot.sendMessage(chat_id, 'Ich weiß nicht, was du von mir willst...')
+        messageList = message.split(' ', 1)
+        if messageList[0] == '/NewNotice' :
+            myNotice = messageList[1]
+            myMessage = bot.sendMessage(chat_id, 'Deine Notiz lautet: ' + myNotice)
+            sureToSave = True
+            bot.sendMessage(chat_id, 'Soll ich diese Notiz aufnehmen?', 'Markdown', False, False, myMessage['message_id'], yesNoKeyboard)
+        elif messageList[0] == '/ShowAll' :
+            n = 'Deine Notizen sind: \n'
+            f = open(str(chat_id) + '.txt', 'r+')
+            fl = f.readlines()
+            i = 1
+            for x in fl :
+                n += str(i) + ': ' + str(x)
+                i += 1
+            bot.sendMessage(chat_id, n)
+            f.close()
+            
+        else :
+            bot.sendMessage(chat_id, 'Entschuldigung, ich weiß nicht, was du von mir willst...')
     
-bot = telepot.Bot('225249137:AAH1X8cObiVtsJvtzOsgIdEtKCt7hX5llL0')    
-yesButton = KeyboardButton()
-yesButton['text'] = '/yes'
-noButton = KeyboardButton()
-noButton['text'] = '/no'
-yesNoKeyboard = ReplyKeyboardMarkup()
-yesNoKeyboard['keyboard'] = [[yesButton], [noButton]]
+bot = telepot.Bot('225249137:AAH1X8cObiVtsJvtzOsgIdEtKCt7hX5llL0')   
+
+global sureToSave
+sureToSave = False 
+#global yesSure = False
+global myNotice 
+#yesButton = KeyboardButton()
+#yesButton['text'] = '/yes'
+#noButton = KeyboardButton()
+#noButton['text'] = '/no'
+global yesNoKeyboard 
+yesNoKeyboard = ReplyKeyboardMarkup(keyboard = [[KeyboardButton(text = '/yes')], [KeyboardButton(text = '/no')]], one_time_keyboard = True)
+#yesNoKeyboard['keyboard'] = [[KeyboardButton(text = '/yes')], [KeyboardButton(text = '/no')]]
 bot.message_loop(handle)
 print ("Ready to set of")
 
