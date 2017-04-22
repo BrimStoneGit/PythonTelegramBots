b59f1000bb530d08cc80c18eefadb3d29252b988
BENNY-LAPTOP\Benny <benjamindornfeld@gmx.de>
2017-04-22 07:48:59 +0200

    Delete Line implemented

--- NoticeBot/Source/NoticeBot.py
+++ NoticeBot/Source/NoticeBot.py
@@ -3,11 +3,14 @@ import time
 import telepot
 import telepot.namedtuple
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+from builtins import str, int
 
 def handle(msg):
     global yesNoKeyboard
     global myNotice
     global sureToSave
+    global sureToDelete
+    global lineToDelete
     content_type, chat_type, chat_id = telepot.glance(msg)
     chat_id = msg['chat']['id']
     if (content_type == 'text') :
@@ -22,49 +25,77 @@ def handle(msg):
     if sureToSave :
         if message == '/yes' :
             bot.sendMessage(chat_id, 'Notiz hinzugefügt')
-            f = open(str(chat_id) + '.txt', 'a+')
-            f.write(myNotice + '\n')
-            f.close()
+            file = open(str(chat_id) + '.txt', 'a+')
+            file.write(myNotice + '\n')
+            file.close()
             myNotice = ''
             sureToSave = False
         else :
             bot.sendMessage(chat_id, 'Notiz verworfen')
             myNotice = ''
             sureToSave = False
+    elif sureToDelete :
+        if message == '/yes' :
+            bot.sendMessage(chat_id, 'Notiz gelöscht')
+            file = open(str(chat_id) + '.txt', 'r')
+            noticeArray = file.readlines()
+            file.close()
+            file = open(str(chat_id) + '.txt', 'w')
+            for i in range(len(noticeArray)) :
+                if i != lineToDelete :
+                    file.write(noticeArray[i])
+            file.close()
+            sureToDelete = False
+        else :
+            bot.sendMessage(chat_id, 'Notiz nicht gelöscht')
+            sureToDelete = False
     else :
         messageList = message.split(' ', 1)
-        if messageList[0] == '/NewNotice' :
-            myNotice = messageList[1]
-            myMessage = bot.sendMessage(chat_id, 'Deine Notiz lautet: ' + myNotice)
-            sureToSave = True
-            bot.sendMessage(chat_id, 'Soll ich diese Notiz aufnehmen?', 'Markdown', False, False, myMessage['message_id'], yesNoKeyboard)
-        elif messageList[0] == '/ShowAll' :
+        if messageList[0] == '/newNotice' :
+            if len(messageList) == 2 :
+                myNotice = messageList[1]
+                myMessage = bot.sendMessage(chat_id, 'Deine Notiz lautet: ' + myNotice)
+                sureToSave = True
+                bot.sendMessage(chat_id, 'Soll ich diese Notiz aufnehmen?', 'Markdown', False, False, myMessage['message_id'], yesNoKeyboard)
+            else :
+                bot.sendMessage(chat_id, 'Bitte "/NewNotice Notiz" eingeben.')
+        elif messageList[0] == '/showAll' :
             n = 'Deine Notizen sind: \n'
-            f = open(str(chat_id) + '.txt', 'r+')
-            fl = f.readlines()
+            file = open(str(chat_id) + '.txt', 'r+')
+            noticeArray = file.readlines()
             i = 1
-            for x in fl :
+            for x in noticeArray :
                 n += str(i) + ': ' + str(x)
                 i += 1
             bot.sendMessage(chat_id, n)
-            f.close()
-            
+            file.close()
+        elif messageList[0] == '/deleteLine' :
+            lineNr = messageList[1].split(' ', 1)
+            delLine = int(lineNr[0])
+            file = open(str(chat_id) + '.txt', 'r+')
+            noticeArray = file.readlines()
+            if len(noticeArray) < delLine :
+                bot.sendMessage(chat_id, 'Du hast nur ' + str(len(noticeArray)) + ' Notizen. Ich kann Zeile ' + str(delLine) + ' nicht löschen.')
+            else :
+                lineToDelete = delLine - 1
+                sureToDelete = True
+                toBeDeleted = noticeArray[delLine - 1].replace("\n", "")
+                bot.sendMessage(chat_id, 'Willst du "' + toBeDeleted + '" (Notiz Nr. ' + str(delLine) + ') wirklich löschen?')
         else :
             bot.sendMessage(chat_id, 'Entschuldigung, ich weiß nicht, was du von mir willst...')
     
 bot = telepot.Bot('225249137:AAH1X8cObiVtsJvtzOsgIdEtKCt7hX5llL0')   
 
+#global Values
 global sureToSave
 sureToSave = False 
-#global yesSure = False
+global sureToDelete
+sureToDelete = False
 global myNotice 
-#yesButton = KeyboardButton()
-#yesButton['text'] = '/yes'
-#noButton = KeyboardButton()
-#noButton['text'] = '/no'
 global yesNoKeyboard 
+global lineToDelete
 yesNoKeyboard = ReplyKeyboardMarkup(keyboard = [[KeyboardButton(text = '/yes')], [KeyboardButton(text = '/no')]], one_time_keyboard = True)
-#yesNoKeyboard['keyboard'] = [[KeyboardButton(text = '/yes')], [KeyboardButton(text = '/no')]]
+
 bot.message_loop(handle)
 print ("Ready to set of")
 
